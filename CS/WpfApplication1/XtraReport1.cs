fdb70a7b9de6226f6e3f992f65dd604863f97095
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WpfApplication1/XtraReport1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+using System.ComponentModel;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraPrinting;
+
+namespace WpfApplication1 {
+    public partial class XtraReport1 : DevExpress.XtraReports.UI.XtraReport {
+        public XtraReport1() {
+            InitializeComponent();
+        }
+
+        private void XtraReport1_BeforePrint(object sender, System.Drawing.Printing.PrintEventArgs e) {
+            PrintingSystem.EditingFieldChanged += new EventHandler<DevExpress.XtraPrinting.EditingFieldEventArgs>(PrintingSystem_EditingFieldChanged);
+        }
+
+        void PrintingSystem_EditingFieldChanged(object sender, DevExpress.XtraPrinting.EditingFieldEventArgs e) {
+            if (((TextEditingField)e.EditingField).EditorName == "DateEditor") {
+                e.EditingField.Brick.Text = string.Format("{0:MMMM dd, yyyy}", System.Convert.ToDateTime(e.EditingField.Brick.Text));
+            }
+        }
+
+    }
+}
---
+++ VB/WpfApplication1/ReportViewModel.vb
@@ -0,0 +1,66 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports DevExpress.Mvvm
+Imports DevExpress.XtraReports.UI
+
+Namespace WpfApplication1
+    Public Class ReportViewModel
+        Inherits BindableBase
+
+        Private privateReport As XtraReport
+        Public Property Report() As XtraReport
+            Get
+                Return privateReport
+            End Get
+            Private Set(ByVal value As XtraReport)
+                privateReport = value
+            End Set
+        End Property
+        Public Sub New(ByVal report As XtraReport)
+            Me.Report = report
+        End Sub
+    End Class
+
+    Public Class PreviewViewModel
+        Inherits ReportViewModel
+
+        Public Sub New(ByVal report As XtraReport)
+            MyBase.New(report)
+            ' Fill the itemsSource of a ComboBox editor.
+            ComboBoxItemsSource = Enumerable.Range(1, 20).Select(Function(x) String.Format("Item {0}", x))
+
+            ' Set the minimum and maximum values for a DateTime editor.
+            DateTimeMin = Date.Now.AddMonths(-2)
+            DateTimeMax = Date.Now.AddMonths(2)
+        End Sub
+
+        Private privateComboBoxItemsSource As IEnumerable(Of String)
+        Public Property ComboBoxItemsSource() As IEnumerable(Of String)
+            Get
+                Return privateComboBoxItemsSource
+            End Get
+            Private Set(ByVal value As IEnumerable(Of String))
+                privateComboBoxItemsSource = value
+            End Set
+        End Property
+        Private privateDateTimeMin As Date
+        Public Property DateTimeMin() As Date
+            Get
+                Return privateDateTimeMin
+            End Get
+            Private Set(ByVal value As Date)
+                privateDateTimeMin = value
+            End Set
+        End Property
+        Private privateDateTimeMax As Date
+        Public Property DateTimeMax() As Date
+            Get
+                Return privateDateTimeMax
+            End Get
+            Private Set(ByVal value As Date)
+                privateDateTimeMax = value
+            End Set
+        End Property
+    End Class
+End Namespace
